f1335ecc991f2128f26f5200eb87ccc5ab9bf26f
NizarIslah <nizar.isl17@gmail.com>
2025-04-17 13:59:44 -0400

    updated parser

--- src/eval_code.py
+++ src/eval_code.py
@@ -26,7 +26,14 @@ import pdb
 import re
 
 def extract_first_python_code_block(text):
-    match = re.search(r"```python(.*?)```", text, re.DOTALL)
+    try:
+        match = re.search(r"```python(.*?)```", text, re.DOTALL)
+    except Exception as e:
+        try:
+            match = re.search(r"```(.*?)```", rf'{text}', re.DOTALL) # anthropic
+        except Exception as e:
+            print("Error: ", e)
+            match = None
     return match.group(1) if match else None
 
 def load_outputs_from_json(options):
