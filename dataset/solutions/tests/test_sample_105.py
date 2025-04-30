a105b5d73b2041bd3ba386e583fc6cda0e4aca0b
Victor May <mayvic@google.com>
2025-04-30 15:14:35 -0400

    fix

--- dataset/solutions/tests/test_sample_105.py
+++ dataset/solutions/tests/test_sample_105.py
@@ -3,6 +3,13 @@ import os
 import sys
 import unittest
 
+from uuid import uuid4
+from django.apps import AppConfig
+
+class MyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'            
+    label = 'myapp_' + str(uuid4())
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Import the module to test
@@ -10,11 +17,16 @@ from sample_105 import Square, create_square, display_side_and_area
 
 
 class TestSquareModel(unittest.TestCase):
+    table_exists = False
+
     def setUp(self):
         # Create the necessary tables in the in-memory database
-        from django.db import connection
-        with connection.schema_editor() as schema_editor:
-            schema_editor.create_model(Square)
+        from django.db import connection       
+        
+        if not TestSquareModel.table_exists:
+            with connection.schema_editor() as schema_editor:
+                schema_editor.create_model(Square)
+            TestSquareModel.table_exists = True
     
     def tearDown(self):
         # Clean up after each test
