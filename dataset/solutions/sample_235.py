56777292473cc357e8ee5a58ffab4091163bbba3
Victor May <mayvic@google.com>
2025-04-30 22:56:39 -0400

    wip

--- dataset/solutions/sample_235.py
+++
@@ -1,6 +0,0 @@
-# library: pytest
-# version: 6.2.0
-# extra_dependencies: []
-import pytest
-
-@pytest.fixture
--- dataset/solutions/sample_236.py
+++
@@ -1,6 +0,0 @@
-# library: pytest
-# version: 7.2.0
-# extra_dependencies: []
-import pytest
-
-@pytest.mark.parametrize("x, y", [(2, 4)])
