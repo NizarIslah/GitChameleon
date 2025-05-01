25012f6981789f3c29727b144b1ee0df5d40f978
Victor May <mayvic@google.com>
2025-04-30 21:58:48 -0400

    wip

--- dataset/solutions/sample_81.py
+++ dataset/solutions/sample_81.py
@@ -4,5 +4,5 @@
 import numpy as np
 import lightgbm as lgb
 from lightgbm import LGBMClassifier
-def predict_start(model: LGBMClassifier, start_iter: int) -> np.ndarray:
+def predict_start(model: LGBMClassifier, data: np.ndarray, start_iter: int) -> np.ndarray:
     return model.predict(data, start_iteration=10) 
