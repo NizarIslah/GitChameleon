4081029e40e6f016e7095d8d33cd9a7624731ef8
Victor May <mayvic@google.com>
2025-05-01 22:06:44 -0400

    wip

--- dataset/solutions/tests/test_sample_39.py
+++ dataset/solutions/tests/test_sample_39.py
@@ -5,33 +5,14 @@ import sys
 import unittest
 import warnings
 
-# Filter deprecation warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Attempt to import gradio; if not available (or if it fails due to environment),
-# we'll skip the tests that depend on it.
-try:
-    import gradio as gr
-    # If we reach here, gradio is importable
-    gradio_import_error = False
-except ImportError:
-    gradio_import_error = True
-    gr = None
-
-# Try to import sample_39 as well, which depends on gradio
-try:
-    if not gradio_import_error:
-        import sample_39
-    else:
-        sample_39 = None
-except ImportError:
-    sample_39 = None
+import gradio as gr
+import sample_39
 
 class TestImageDisplay(unittest.TestCase):
     """Test cases for the display_image function and Gradio Interface in sample_39.py."""
 
-    @unittest.skipIf(gradio_import_error or sample_39 is None, 
-                     "Skipping because gradio (or sample_39) could not be imported.")
     def test_display_image_returns_correct_url(self):
         """Test that display_image returns the correct image URL."""
         expected_url = "https://image_placeholder.com/42"
